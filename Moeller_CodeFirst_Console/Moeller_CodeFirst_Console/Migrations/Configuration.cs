6f9eeb676b6fb86d1633162d41ebc98c0682a761
Jakob Ladegaard Møller <jlm1999@live.dk>
2017-02-07 11:03:38 +0100

    Not sure what i did

---
+++ Moeller_CodeFirst_Console/Moeller_CodeFirst_Console/Migrations/Configuration.cs
@@ -0,0 +1,32 @@
+namespace Moeller_CodeFirst_Console.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Moeller_CodeFirst_Console.Program.BlogContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+            ContextKey = "Moeller_CodeFirst_Console.Program+BlogContext";
+        }
+
+        protected override void Seed(Moeller_CodeFirst_Console.Program.BlogContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
+            //  to avoid creating duplicate seed data. E.g.
+            //
+            //    context.People.AddOrUpdate(
+            //      p => p.FullName,
+            //      new Person { FullName = "Andrew Peters" },
+            //      new Person { FullName = "Brice Lambson" },
+            //      new Person { FullName = "Rowan Miller" }
+            //    );
+            //
+        }
+    }
+}
---
+++ Moeller_CodeFirst_Console/Moeller_CodeFirst_Console/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Moeller_CodeFirst_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (BlogContext db = new BlogContext())
+            {
+                ICollection<Blog> query = (ICollection<Blog>) from b in db.Blogs orderby b.Name select b;
+
+                foreach (Blog item in query)
+                {
+                    Console.WriteLine(item.Name);
+                }
+
+                Console.Write("Enter blog name: ");
+                string name = Console.ReadLine();
+
+                Blog blog = new Blog { Name = name };
+                db.Blogs.Add(blog);
+                db.SaveChanges();
+            }
+        }
+
+        public class Blog
+        {
+            public int BlogId { get; set; }
+            public string Name{ get; set; }
+
+            public virtual List<Post> Posts { get; set; }
+        }
+
+        public class Post
+        {
+            public int PostId { get; set; }
+            public string Title { get; set; }
+            public string Content { get; set; }
+
+            public int BlogId { get; set; }
+            public virtual Blog Blog { get; set; }
+        }
+
+        public class BlogContext : DbContext
+        {
+            public DbSet<Blog> Blogs { get; set; }
+            public DbSet<Post> Posts { get; set; }
+        }
+    }
+}
