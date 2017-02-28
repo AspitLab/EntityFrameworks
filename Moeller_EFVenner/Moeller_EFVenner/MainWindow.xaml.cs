00bf5d0ce373f0807bea5815757191acca5fb9bb
Jakob Ladegaard Møller <jlm1999@live.dk>
2017-02-28 12:12:09 +0100

    implemented jens' solution into my own

--- Moeller_EFVenner/Moeller_EFVenner/MainWindow.xaml.cs
+++ Moeller_EFVenner/Moeller_EFVenner/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +21,32 @@ namespace Moeller_EFVenner
     /// </summary>
     public partial class MainWindow : Window
     {
+        private ClassBizz cb = new ClassBizz();
+        private ClassFriend classFriend = new ClassFriend();
         public MainWindow()
         {
             InitializeComponent();
+            this.listViewFriend.DataContext = cb;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Data.CollectionViewSource mainVenneTabelViewSource = ((System.Windows.Data.CollectionViewSource)(this.FindResource("mainVenneTabelViewSource")));
+            cb.UpdateFriend();
+        }
+
+        private void listViewFriend_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            var lvi = (sender as ListView).SelectedItem;
+            if (lvi != null)
+            {
+                ClassFriend cv = lvi as ClassFriend;
+
+                MessageBox.Show(cv.friendData.fornavn.ToString() + " " + cv.friendData.efternavn.ToString() + "\n" +
+                    cv.friendData.adresse.ToString() + "\n" +
+                    cv.friendData.postNr.ToString() + " " + cv.friendData.PostByTabel.byNavn.ToString() + "\n" +
+                    cv.friendNewTelefon[1].strTelefonNr.ToString() + " " + cv.friendNewTelefon[1].strTelefonType.ToString());
+            }
         }
     }
 }
